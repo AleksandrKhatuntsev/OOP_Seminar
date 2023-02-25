7b14b2a76d391e4af4027c33d2b0d26934971fb4
AleksandrKhatuntsev <hatuntsev.alexander@yandex.ru>
2023-02-25 17:50:41 +0300

    Создание класса Fruit

---
+++ Fruit.java
@@ -0,0 +1,15 @@
+/*
+ * Создать класс Fruit
+ * В классе должна храниться следующая информация:
+ * Название-name
+ * Цвет-color
+ * вес-weight
+ * стоимость-price
+ */
+
+ public class Fruit {
+    String name;
+    String color;
+    double weight;
+    double price;
+ }
